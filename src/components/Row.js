31cc08dfba9a4e829edaa616fa7e175deb874b16
2JoongHo <honor9320@gmail.com>
2026-01-10 00:58:24 +0900

    42 모달 생성

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,53 +1,76 @@
-import React, { useEffect, useState } from 'react';
-import axios from '../api/axios';
-import './Row.css';
+import React, { useEffect, useState } from "react";
+import axios from "../api/axios";
+import "./Row.css";
+import MovieModal from "./MovieModal";
 
-export default function Row({title, id, fetchUrl, isLargeRow}) {
+export default function Row({ title, id, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
 
-    const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
 
-    useEffect(() => {
-        fetchMovieData();
-    }, []);
+  const [movieSelected, setMovieSelected] = useState({});
 
-    const fetchMovieData = async () => {
-        const request = await axios.get(fetchUrl);
-        console.log('request', request);
-        setMovies(request.data.results);
-    }
+  useEffect(() => {
+    fetchMovieData();
+  }, []);
 
-    return <section className="row">
-        <h2>{title}</h2>
-        <div className="slider">
-            <div className="slider_arrow-left">
-                <span className="arrow"
-                onClick={() => {
-                    document.getElementById(id).scrollLeft -= window.innerWidth -80;
-                }}>
-                    {"<"}
-                </span>
-            </div>
+  const fetchMovieData = async () => {
+    const request = await axios.get(fetchUrl);
+    console.log("request", request);
+    setMovies(request.data.results);
+  };
 
-            <div id={id} className="row_poster">
-                {movies.map(movie => (
-                    <img
-                        key={movie.id}
-                        className={`row_poster ${isLargeRow && "row_posterLarge"}`}
-                        src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path: movie.backdrop_path}`}
-                        alt={movie.name}
-                    />
-                ))}
+  const handleClick = (movie) => {
+    setModalOpen(true);
+    setMovieSelected(movie);
+  };
 
-            </div>
-
-            <div className="slider_arrow-right">
-                    <span className="arrow"
-                    onClick={() => {
-                        document.getElementById(id).scrollRight += window.innerWidth -80;
-                    }}>
-                        {">"}
-                    </span>
-            </div>
+  return (
+    <section className="row">
+      <h2>{title}</h2>
+      <div className="slider">
+        <div className="slider_arrow-left">
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+            }}
+          >
+            {"<"}
+          </span>
         </div>
+
+        <div id={id} className="row_poster_container">
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              className={`row_poster_item ${
+                isLargeRow && "row_poster_item_Large"
+              }`}
+              src={`https://image.tmdb.org/t/p/original/${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+              onClick={() => handleClick(movie)}
+            />
+          ))}
+        </div>
+
+        <div className="slider_arrow-right">
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft += window.innerWidth - 80;
+            }}
+          >
+            {">"}
+          </span>
+        </div>
+      </div>
+
+      {modalOpen && (
+        <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
+      )}
     </section>
+  );
 }
