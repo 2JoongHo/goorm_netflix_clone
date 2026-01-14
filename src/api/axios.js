b7437433f5ff814ec1abf87899da20ffed1b8ab8
Joongho <honor9320@gmail.com>
2026-01-14 12:10:23 +0900

    배포 준비

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3",
     params:{
-        api_key: "7b9921791adc1df298f533958a093af0",
+        api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
         language: "ko-KR",
     }
 })
